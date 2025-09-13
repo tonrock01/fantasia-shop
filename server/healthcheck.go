9bcc3ec99a005ef198aa137ea67d29e28abdb153
tonrock01 <85146711+tonrock01@users.noreply.github.com>
2025-09-13 22:27:12 +0700

    initial module and health check

--- server/healthcheck.go
+++ server/healthcheck.go
@@ -1,1 +1,20 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/tonrock01/fantasia-shop/pkg/response"
+)
+
+type healthCheck struct {
+	App    string `json:"app"`
+	Status string `json:"status"`
+}
+
+func (s *server) healthCheckService(c echo.Context) error {
+	return response.SuccessResponse(c, http.StatusOK, &healthCheck{
+		App:    s.cfg.App.Name,
+		Status: "OK",
+	})
+}
